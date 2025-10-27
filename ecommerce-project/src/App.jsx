99c7919b3afbc2e703a8985767ebf7a2f78df2e2
Amer Harb <amerharb@hotmail.com>
2025-10-27 12:10:28 -0700

    adding Router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
     return (
-   <HomePage/>
+    <Routes>
+      <Route index element={<HomePage/>}/> {/* index = path="/"*/}
+      <Route path="/checkout" element={<HomePage/>}/>
+    </Routes>
   )
 }
 
