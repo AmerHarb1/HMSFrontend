63877fcd48d53ed5bf7bca5c9d5370a6b8584b11
Amer Harb <amerharb@hotmail.com>
2025-10-27 12:41:42 -0700

    added checkoutpage

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import './HomePage.css';
 export function HomePage(){
     return (
         <>
+            <title>Ecommers Project</title>
             <div className="header">
                 <div className="left-section">
                     <a href="index.html" className="header-link">
