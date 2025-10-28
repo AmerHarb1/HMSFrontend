c195946f168b0f1c8869d8763edd6728c0f84dba
Amer Harb <amerharb@hotmail.com>
2025-10-28 14:13:48 -0700

    get cart from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,10 +5,17 @@ import './HomePage.css';
 
 export function HomePage(){
     const [products, setProducts] = useState([]);  //useState has two props the first is a varial and the second is a function to update the variable
+    const [cart, setCart] = useState([]);
+    
     useEffect(()=>{
         axios.get('http://localhost:3000/api/products') // we place this code inside useEffect to make it run once, useEffect controls how many time a code inside it would run
             .then((response)=> {
                 setProducts(response.data);    //set products once response has data  
+            });
+
+        axios.get('http://localhost:3000/api/cart-items') // get the cart data
+            .then((response)=> {
+                setCart(response.data);    //set cart once response has data  
             });
     },[]);  //[] this array controls the run frequenccy, empty means run once
     
@@ -17,7 +24,7 @@ export function HomePage(){
         <>
             <title>Ecommers Project</title>
 
-            <Header/>
+            <Header cart = {cart}/>
 
             <div className="home-page">
                 <div className="products-grid">
